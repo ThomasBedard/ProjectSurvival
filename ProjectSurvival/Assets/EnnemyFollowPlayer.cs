866d445c941a0bcb0f2c707fc04b2ed198507ebf
HennCH <2132132@champlaincollege.qc.ca>
2023-05-03 09:54:55 -0400

    Cat ennemy
    Trying to make the cat follow the player

--- ProjectSurvival/Assets/EnnemyFollowPlayer.cs
+++ ProjectSurvival/Assets/EnnemyFollowPlayer.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class EnnemyFollowPlayer : MonoBehaviour
 {
+    public Transform player;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("player_0").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
     }
 }
