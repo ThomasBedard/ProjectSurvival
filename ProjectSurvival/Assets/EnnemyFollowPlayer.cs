136f38238c4aec5aadeffe2e6add595288a0e853
HennCH <2132132@champlaincollege.qc.ca>
2023-05-03 10:54:49 -0400

    Added details
    I fixed a small detail on the tilemap and made the cat follow the player. I'm trying to make something happen when the player takes the gem

--- ProjectSurvival/Assets/EnnemyFollowPlayer.cs
+++ ProjectSurvival/Assets/EnnemyFollowPlayer.cs
@@ -5,17 +5,49 @@ using static UnityEngine.GraphicsBuffer;
 
 public class EnnemyFollowPlayer : MonoBehaviour
 {
-    public Transform player;
+    private Transform player;
+
+    private Rigidbody2D rb2d;
+
     public float speed;
+
+    private Vector2 move;
+
+    private Vector3 previousPosition;
     // Start is called before the first frame update
     void Start()
     {
+        //StartCoroutine(wait());
+        rb2d = GetComponent<Rigidbody2D>();
         player = GameObject.Find("player_0").transform;
+        previousPosition = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+        if (player.position != previousPosition)
+        {
+            if (player)
+            {
+                Vector3 direction = (player.position - transform.position).normalized;
+                move = direction;
+            }
+        }
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (player)
+        {
+            rb2d.velocity = new Vector2(move.x, move.y) * speed;
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSecondsRealtime(3);
     }
 }
