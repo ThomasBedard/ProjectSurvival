8e3957606e04ed7749812d959d284d6b42ce4630
HennCH <2132132@champlaincollege.qc.ca>
2023-05-03 23:24:03 -0400

    l
    l

--- ProjectSurvival/Assets/Player/MoveCharacter.cs
+++ ProjectSurvival/Assets/Player/MoveCharacter.cs
@@ -9,9 +9,12 @@ public class MoveCharacter : MonoBehaviour
 {
     //link for character -> https://stackoverflow.com/questions/45569580/sprite-sheet-animation-with-arrow-keys
     //link for gem -> https://www.deviantart.com/bigbark24/art/Diamond-sprite-sheet-759155287
+<<<<<<< HEAD
     //link for level 1 sprite sheet -> https://opengameart.org/content/outdoor-tiles-again
     //link for tree -> https://opengameart.org/content/gnarled-tree
     //link for level 2 sprite sheet -> https://opengameart.org/content/sci-fi-interior-tiles
+=======
+>>>>>>> parent of 22564a0 (Background + tiles)
     Animator animator; //to talk to the animator
     public Rigidbody2D rb;
     public float moveSpeed = 7f;
